c8fd5894460af92c8759c2f1929fd51e69c64c9b
Mike Turley <mike@miketurley.com>
2012-11-16 14:16:35 -0500

    building active graph toggle into the menu.  still need delete function, and integration with url hash.

--- client/client.js
+++ client/client.js
@@ -1,11 +1,39 @@
 // Meteor.subscribe calls go in here
 
 Template.body.events({
- 'click .button_new-automaton' : function() {
-   alert("Coming Soon!");
+  'click .button_new-automaton' : function() {
+    Session.set('activeGraph',Graphs.insert({
+      'type'          : 'automaton',
+      'deterministic' : 'false',
+      'owner'         : Meteor.userId(),
+      'title'         : 'Untitled Automaton'
+    }));
+  },
+  'click #myautomata li a' : function() {
+    if(this._id != Session.get('activeGraph')) {
+      Session.set('activeGraph',this._id);
+    } else {
+      Session.set('activeGraph',null);
+    }
   }
 });
 
+Template.body.myautomata = function() {
+  return Graphs.find({'owner' : Meteor.userId()});
+};
+
+Template.body.isActiveGraph = function() {
+  return this._id == Session.get('activeGraph');
+};
+
+Template.body.activeGraphExists = function() {
+  return Session.get('activeGraph') != null;
+};
+
+Template.body.activeGraph = function() {
+  return Graphs.findOne({'_id':Session.get('activeGraph')});
+};
+
 Meteor.startup(function() {
-  /// startup behavior here?
+  Session.set('activeGraph',null);
 });

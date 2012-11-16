112d81bace609fcc4a18aedff280d79c0f5a5c9e
Mike Turley <mike@miketurley.com>
2012-11-16 13:36:51 -0500

    fleshing out models

--- model.js
+++ model.js
@@ -1,2 +1,52 @@
-Automata = new Meteor.Collection("automata");
-StateVertices = new Meteor.Collection("stateVertices");
+Graphs = new Meteor.Collection("graphs");
+/*  Example Graph Object: {
+  'type'          : 'automaton'
+  'deterministic' : false,
+  'owner'         : <user account id>,
+  'title'         : 'My First Automaton'
+}  */
+Vertices = new Meteor.Collection("vertices");
+/* Example Vertex Object: {
+  'graph_id' : <graph id>,
+  'label'    : 'p',
+  'colors'   : {
+    'start' : true,
+    'final' : false,
+  },
+  'xpos' : 35,
+  'ypos' : 35
+} */
+Edges = new Meteor.Collection("edges");
+/* Example Edge Object: {
+  'graph_id'    : <graph_id>,
+  'directed'    : true,
+  'fromvert_id' : <vertex_id>,
+  'tovert_id'   : <vertex_id>,
+  'label'       : '1',
+  'positioning' : 'direct'
+} */
+
+Simulations = new Meteor.Collection("simulations");
+/* Example Simulation object: {
+  'graph_id'     : <graph_id>,
+  'input'        : '10010101001',
+  'currentIndex' : 3
+  'currentState' : <vertex_id>,
+  'history' : [
+    {
+      'oldState'   : null,
+      'inputIndex' : 0,
+      'inputChar'  : '1',
+      'newState'   : <vertex_id>,
+      'atStart'    : true,
+      'atFinal'    : false
+    }, {
+      'oldState'   : null,
+      'inputIndex' : 1,
+      'inputChar'  : '0',
+      'newState'   : <vertex_id>,
+      'atStart'    : false,
+      'atFinal'    : false
+    }
+  ]
+} */

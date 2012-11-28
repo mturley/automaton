dfdac466a08521501ba4a992395b076b1016e02c
Mike Turley <mike@miketurley.com>
2012-11-28 03:41:01 -0500

    improving delete functionality

--- client/client.js
+++ client/client.js
@@ -1,29 +1,5 @@
 // Meteor.subscribe calls go in here
 
-Template.body.events({
-  'click .button_new-automaton' : function() {
-    Session.set('activeGraph',Graphs.insert({
-      'type'          : 'automaton',
-      'deterministic' : 'false',
-      'owner'         : Meteor.userId(),
-      'title'         : 'Untitled Automaton'
-    }));
-  },
-  'click #myautomata li a' : function() {
-    if(this._id != Session.get('activeGraph')) {
-      Session.set('activeGraph',this._id);
-    } else {
-      Session.set('activeGraph',null);
-    }
-  },
-  'click .button_delete-automaton' : function() {
-    if(confirm("Are you sure you want to delete this automaton?")) {
-      Graphs.remove({'_id':Session.get('activeGraph')});
-      Session.set('activeGraph',null);
-    }
-  }
-});
-
 Template.body.myautomata = function() {
   return Graphs.find({'owner' : Meteor.userId()});
 };
@@ -40,6 +16,42 @@ Template.body.activeGraph = function() {
   return Graphs.findOne({'_id':Session.get('activeGraph')});
 };
 
+Template.body.events({
+  'click .button_new-automaton' : function() {
+    Session.set('activeGraph',Graphs.insert({
+      'type'          : 'automaton',
+      'deterministic' : 'false',
+      'owner'         : Meteor.userId(),
+      'title'         : 'Untitled Automaton'
+    }));
+  },
+  'click #myautomata li a' : function() {
+    if(this._id != Session.get('activeGraph')) {
+      Session.set('activeGraph',this._id);
+    } else {
+      Session.set('activeGraph',null);
+    }
+  },
+  'click #myautomata li .delete-icon' : function() {
+    Session.set('dialogContextGraph', this._id)
+    $('#confirm-delete-dialog').modal('show');
+  }
+});
+
+Template.hiddenElements.events({
+  'click #confirm-delete-dialog .btn-danger' : function() {
+    var g = Session.get('dialogContextGraph');
+    Graphs.remove({'_id':g});
+    if(Session.get('activeGraph') == g)
+      Session.set('activeGraph',null);
+    $('#confirm-delete-dialog').modal('hide');
+  },
+  'click #confirm-delete-dialog .btn-secondary' : function() {
+    $('#confirm-delete-dialog').modal('hide');
+  }
+});
+
 Meteor.startup(function() {
   Session.set('activeGraph',null);
+  $('#confirm-delete-dialog').modal({ backdrop: true, show: false });
 });
